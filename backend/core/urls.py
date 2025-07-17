4f81a92177f539901e06ba61ff04890db911c537
Łukasz Piasecki <80245965+LukaszPiasecki13@users.noreply.github.com>
2025-07-17 23:48:51 +0200

    BACKEND: authentication

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
     path('reports/', include('reports.urls')),
+    path('auth/', include('authentication.urls')),
 ]
